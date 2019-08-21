c04e1c1b2525c5143e7e88b302c139209d990daa
Stefanos Papanatsios <spapanatsios@gmail.com>
2019-08-21 16:07:43 +0300

    Account avatar now uses user picture if one exists

--- components/NavBar.js
+++ components/NavBar.js
@@ -7,6 +7,7 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
+import Avatar from '@material-ui/core/Avatar'
 import AccountCircle from '@material-ui/icons/AccountCircle'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger'
 import List from '@material-ui/core/List'
@@ -14,7 +15,7 @@ import Divider from '@material-ui/core/Divider'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
-import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button'
 import HomeIcon from '@material-ui/icons/Home'
 import BookIcon from '@material-ui/icons/Book'
 import SettingsIcon from '@material-ui/icons/Settings'
@@ -53,6 +54,10 @@ const useStyles = makeStyles(theme => ({
   },
   fullList: {
     width: 'auto'
+  },
+  avatar: {
+    width: '32px',
+    height: '32px'
   }
 }))
 
@@ -158,7 +163,11 @@ export default function (props) {
                 component={ButtonLink}
                 href='user'
               >
-                <AccountCircle />
+                {user.picture ? (
+                  <Avatar alt='account picture' src={user.picture} className={classes.avatar} />
+                ) : (
+                  <AccountCircle className={classes.avatar} />
+                )}
               </IconButton>
             ) : (
               <Button color='inherit' href='login'>Login</Button>
