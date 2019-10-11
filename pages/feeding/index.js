b0f0db25220eda82d1890c8136da293def47414d
Orff <orfous29@gmail.com>
2019-10-11 16:40:16 +0300

    Implemented feeding page vol2

--- pages/feeding/index.js
+++ pages/feeding/index.js
@@ -4,20 +4,57 @@ import TextField from '@material-ui/core/TextField'
 import Container from '@material-ui/core/Container'
 import gql from '../../scripts/graphql'
 import MenuItem from '@material-ui/core/MenuItem'
+import PropTypes from 'prop-types'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+
+function TabPanel (props) {
+  const { children, value, index, ...other } = props
+
+  return (
+    <Typography
+      component='div'
+      role='tabpanel'
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Box p={7}>{children}</Box>
+    </Typography>
+  )
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired
+}
+
+function a11yProps (index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`
+  }
+}
 
 const feedingHandler = () => Promise.resolve(
   gql(`
       query {
-        feeding {
-          _id
-          days {
-            meals {
-              time
-              menu
-            }
-          }
+   feeding {
+    weeks {
+      days {
+        meals {
+          time
+          menu
         }
       }
+    }
+  }
+}
     `).then(data => data.feeding)
 )
 
@@ -49,6 +86,7 @@ export default function Index (props) {
   const [values, setValues] = useState({
     selectedFeedingId: ''
   })
+  const [value, setValue] = React.useState(0)
 
   useEffect(() => {
     props.updateTitle('Feeding')
@@ -61,6 +99,10 @@ export default function Index (props) {
 
   const handleChange = name => event => {
     setValues({ ...values, [name]: event.target.value })
+  }
+
+  const handleChange2 = (event, newValue) => {
+    setValue(newValue)
   }
 
   return (
@@ -88,6 +130,40 @@ export default function Index (props) {
           ))}
         </TextField>
       </form>
+      <div className={classes.root}>
+        <AppBar position='static'>
+          <Tabs value={value} onChange={handleChange2} aria-label='simple tabs example' variant='scrollable' scrollButtons='on'>
+            <Tab label='Day One' {...a11yProps(0)} />
+            <Tab label='Day Two' {...a11yProps(1)} />
+            <Tab label='Day Three' {...a11yProps(2)} />
+            <Tab label='Day Four' {...a11yProps(3)} />
+            <Tab label='Day Five' {...a11yProps(4)} />
+            <Tab label='Day Six' {...a11yProps(5)} />
+            <Tab label='Day Seven' {...a11yProps(6)} />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={value} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          Item Four
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          Item Five
+        </TabPanel>
+        <TabPanel value={value} index={5}>
+          Item Six
+        </TabPanel>
+        <TabPanel value={value} index={6}>
+          Item Seven
+        </TabPanel>
+      </div>
     </Container>
   )
 }
