f5900f089f47440d5b36610f0a74b81f39c2a548
jim3692 <jim3692@gmail.com>
2020-08-09 17:06:22 +0300

    Server errors rework

--- server/routes/session.js
+++ server/routes/session.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const { ErrorHandler } = require('../lib/error')
+
 const router = express.Router()
 
 router.get(
   '/profile',
-  (req, res) => {
+  (req, res, next) => {
     const profile = (req.user && req.user.profile) || null
-    res.json(profile)
+    if (profile === null) {
+      return next(new ErrorHandler(
+        401,
+        'Not authenticated'
+      ))
+    }
+
+    return res.json(profile)
   }
 )
 
