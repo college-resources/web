437978688d17e87ddabae25f19b3373a895061fc
Orff <orfous29@gmail.com>
2020-08-13 22:14:34 +0300

    Notes page selection display test

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,9 +1,11 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from '@material-ui/core/Container'
 import { dynamicSortMultiple } from '../../scripts/sorting'
 import gql from '../../scripts/graphql'
 import TextField from '@material-ui/core/TextField'
 import Autocomplete from '@material-ui/lab/Autocomplete'
+import Paper from '@material-ui/core/Paper'
+import { makeStyles } from '@material-ui/core/styles'
 
 const lessonHandler = () => Promise.resolve(gql(`
   query {
@@ -26,25 +28,26 @@ const lessonHandler = () => Promise.resolve(gql(`
   'lessonCode'
 ))))
 
-const notesHandler = () => Promise.resolve(gql(`
-    query {
-      lessons {
-        _id
-        name
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(160),
+      height: theme.spacing(80)
     }
-    }
-    `).then((data) => data.lessonNotes && data.lessonNotes.sort(dynamicSortMultiple(
-  'name'
-))))
+  }
+}))
 
 export default function CoursesPage (props) {
   const [
     lessons,
     setLessons,
-    lessonNotes,
-    setLessonNotes
   ] = useState([])
-
+  const options = ['Option 1', 'Option 2']
+  const [value, setValue] = React.useState(options[0])
+  const [inputValue, setInputValue] = React.useState('')
+  const classes = useStyles()
   useEffect(
     () => {
       props.updateTitle('Notes')
@@ -56,31 +59,29 @@ export default function CoursesPage (props) {
     },
     []
   )
-
-  useEffect(
-    () => {
-      notesHandler().then((gqlLessonNotes) => {
-        if (gqlLessonNotes) {
-          setLessonNotes(gqlLessonNotes)
-        }
-      })
-    },
-    []
-  )
-
   return (
     <Container>
       <Autocomplete
         getOptionLabel={(option) => option.name}
         id='combo-box-demo'
-        options={lessonNotes}
+        options={options}
         renderInput={(params) => (<TextField
           {...params}
+          fullwidth
           label='Combo box'
           variant='outlined'
-          fullWidth
-        />)}
+          value={value}
+          onChange={(event, newValue) => { setValue(newValue) }}
+          inputValue={inputValue}
+          onInputChange={(event, newInputValue) => { setInputValue(newInputValue) }} />
+        )}
+
       />
+      <div>{`value: ${value !== null ? `'${value}'` : 'null'}`}</div>
+      <div>{`inputValue: '${inputValue}'`}</div>
+      <div className={classes.root}>
+        <Paper variant='outlined' />
+      </div>
     </Container>
   )
 }
