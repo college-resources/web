d2d09b712120a7a63ae060ef53ac82d52f987459
feedmycat <konstantangelou@gmail.com>
2020-08-21 19:16:07 +0300

    Remove unneeded meta tags

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -29,12 +29,8 @@ class MyDocument extends Document {
           <meta content={themeColor} name="theme-color" />
           <meta charSet='utf-8' />
           <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-          <meta
-            name='viewport'
-            content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
           <meta name='description' content='Description' />
           <meta name='keywords' content='Keywords' />
-          <title>CoRes PWA</title>
           <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
           <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
           <link rel='apple-touch-icon' href='/apple-icon.png' />
