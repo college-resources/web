5b97627d283470fcc59d2cafc684c5b3ba65832a
CrazyStevenz <spapanatsios@gmail.com>
2020-08-24 19:55:29 +0300

    Streamline favicons

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,31 +8,30 @@ class MyDocument extends Document {
   render () {
     return (
       // TODO automatic lang selection
-      <html lang='en'>
+      <html lang="en">
         <Head>
-          <link rel='apple-touch-icon' href='/apple-icon.png' />
-          <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
-          <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
-          <link href='/apple-icon-57x57.png' rel='apple-touch-icon' sizes='57x57' />
-          <link href='/apple-icon-60x60.png' rel='apple-touch-icon' sizes='60x60' />
-          <link href='/apple-icon-72x72.png' rel='apple-touch-icon' sizes='72x72' />
-          <link href='/apple-icon-76x76.png' rel='apple-touch-icon' sizes='76x76' />
-          <link href='/apple-icon-114x114.png' rel='apple-touch-icon' sizes='114x114' />
-          <link href='/apple-icon-120x120.png' rel='apple-touch-icon' sizes='120x120' />
-          <link href='/apple-icon-144x144.png' rel='apple-touch-icon' sizes='144x144' />
-          <link href='/apple-icon-152x152.png' rel='apple-touch-icon' sizes='152x152' />
-          <link href='/apple-icon-180x180.png' rel='apple-touch-icon' sizes='180x180' />
-          <link href='/android-icon-192x192.png' rel='icon' sizes='192x192' type='image/png' />
-          <link href='/favicon-32x32.png' rel='icon' sizes='32x32' type='image/png' />
-          <link href='/favicon-96x96.png' rel='icon' sizes='96x96' type='image/png' />
-          <link href='/favicon-16x16.png' rel='icon' sizes='16x16' type='image/png' />
-          <link href='/manifest.json' rel='manifest' />
-          <meta content={themeColor} name='msapplication-TileColor' />
-          <meta content='/ms-icon-144x144.png' name='msapplication-TileImage' />
-          <meta content={themeColor} name='theme-color' />
-          <meta charSet='utf-8' />
-          <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-          <meta name='description' content='The go-to place for all your college needs.' />
+          <meta charSet="utf-8" />
+          <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+          <meta content="The go-to place for all your college needs." name="description" />
+          <meta content={themeColor} name="theme-color" />
+
+          {/* Appears as a tile, just like a native Windows app in Windows 8 and 10 */}
+          <meta content="#2d89ef" name="msapplication-TileColor" />
+
+          {/* Standard for most desktop browsers */}
+          <link crossOrigin="anonymous" href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+          <link crossOrigin="anonymous" href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+
+          {/* Apple devices https://realfavicongenerator.net/blog/how-ios-scales-the-apple-touch-icon/ */}
+          <link crossOrigin="anonymous" href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+
+          {/* Safari implements pinned tabs and takes advantage of the MacBook Touch Bar.
+              This feature relies on an SVG icon. This icon must be monochrome and Safari does the rest. */}
+          <link color={themeColor} crossOrigin="anonymous" href="/safari-pinned-tab.svg" rel="mask-icon" />
+
+          {/* Google Developer Web App Manifest Recommendation */}
+          <link crossOrigin="anonymous" href="/android-chrome-192x192.png" rel="icon" sizes="192x192" type="image/png" />
+          <link crossOrigin="anonymous" href="/manifest.json" rel="manifest" />
         </Head>
         <body>
           <Main />
