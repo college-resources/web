722d8a91aa8a7556a8ab79a5e6ddcd92e84aa7f0
CrazyStevenz <spapanatsios@gmail.com>
2020-08-24 20:04:26 +0300

    Remove unnecessary crossOrigin from links

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -19,19 +19,19 @@ class MyDocument extends Document {
           <meta content="#2d89ef" name="msapplication-TileColor" />
 
           {/* Standard for most desktop browsers */}
-          <link crossOrigin="anonymous" href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
-          <link crossOrigin="anonymous" href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+          <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+          <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
 
           {/* Apple devices https://realfavicongenerator.net/blog/how-ios-scales-the-apple-touch-icon/ */}
-          <link crossOrigin="anonymous" href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+          <link href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
 
           {/* Safari implements pinned tabs and takes advantage of the MacBook Touch Bar.
               This feature relies on an SVG icon. This icon must be monochrome and Safari does the rest. */}
-          <link color={themeColor} crossOrigin="anonymous" href="/safari-pinned-tab.svg" rel="mask-icon" />
+          <link color={themeColor} href="/safari-pinned-tab.svg" rel="mask-icon" />
 
           {/* Google Developer Web App Manifest Recommendation */}
-          <link crossOrigin="anonymous" href="/android-chrome-192x192.png" rel="icon" sizes="192x192" type="image/png" />
-          <link crossOrigin="anonymous" href="/manifest.json" rel="manifest" />
+          <link href="/android-chrome-192x192.png" rel="icon" sizes="192x192" type="image/png" />
+          <link href="/manifest.json" rel="manifest" />
         </Head>
         <body>
           <Main />
