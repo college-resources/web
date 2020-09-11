5df6015d039d1b8665ee5e002bda0fd39c63c146
Orfous <53450327+Orfous@users.noreply.github.com>
2020-09-12 02:02:14 +0300

    Course Notes page  (#35)
    Add Course Notes page

---
+++ components/notes/Dialog.jsx
@@ -0,0 +1,140 @@
+import CloseIcon from '@material-ui/icons/Close'
+import Dialog from '@material-ui/core/Dialog'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import IconButton from '@material-ui/core/IconButton'
+import Markdown from 'markdown-to-jsx'
+import MuiDialogTitle from '@material-ui/core/DialogTitle'
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import { withStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles({
+  root: {
+    '& .MuiDialog-paperWidthLg.MuiDialog-paperScrollBody': {
+      margin: 0,
+      maxWidth: '1280px',
+      width: 'calc(100% - 32px)'
+    }
+  },
+  content: {
+    padding: '8px 16px'
+  },
+  image: {
+    maxWidth: '100%'
+  }
+})
+
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2)
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500]
+  }
+})
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props
+  return (
+    <MuiDialogTitle
+      className={classes.root}
+      disableTypography
+      style={{ wordWrap: 'break-word', width: 'calc(100% - 64px)' }}
+      {...other}
+    >
+      <Typography
+        variant="h6"
+      >
+        {children}
+      </Typography>
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  )
+})
+
+export default function ScrollDialog (props) {
+  const classes = useStyles()
+  const { open, setOpen, texts, title, images } = props
+
+  function handleClose () {
+    setOpen(false)
+  }
+
+  const descriptionElementRef = React.useRef(null)
+  React.useEffect(
+    () => {
+      if (open) {
+        const { current: descriptionElement } = descriptionElementRef
+        if (descriptionElement !== null) {
+          descriptionElement.focus()
+        }
+      }
+    },
+    [open]
+  )
+
+  return (
+    <Dialog
+      aria-describedby="scroll-dialog-description"
+      aria-labelledby="scroll-dialog-title"
+      className={classes.root}
+      fullWidth
+      maxWidth="lg"
+      onClose={handleClose}
+      open={open}
+      scroll="body"
+    >
+      <DialogTitle
+        id="customized-dialog-title"
+        onClose={handleClose}
+      >
+        {title}
+      </DialogTitle>
+      <DialogContent
+        className={classes.content}
+        dividers
+      >
+        <DialogContentText
+          id="scroll-dialog-description"
+          ref={descriptionElementRef}
+          tabIndex={-1}
+        >
+          {
+            texts.map((text, index) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <p key={`text-${index}`}>
+                <Markdown>
+                  {text}
+                </Markdown>
+              </p>
+            ))
+          }
+        </DialogContentText>
+        {
+          images && images.map((image, index) => (
+            <img
+              className={classes.image}
+              // eslint-disable-next-line react/no-array-index-key
+              key={`image-${index}`}
+              src={image.details.url || image.url}
+            />
+          ))
+        }
+      </DialogContent>
+    </Dialog>
+  )
+}
---
+++ pages/notes/index.jsx
@@ -0,0 +1,201 @@
+import React, { useEffect, useState } from 'react'
+import Autocomplete from '@material-ui/lab/Autocomplete'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardContent from '@material-ui/core/CardContent'
+import Container from '@material-ui/core/Container'
+import Dialog from 'components/notes/Dialog'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import { dynamicSortMultiple } from 'scripts/sorting'
+import gql from 'scripts/graphql'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minWidth: 100,
+    marginTop: 16
+  },
+  title: {
+    fontSize: 14
+  },
+  autocomplete: {
+    '& .MuiOutlinedInput-root': {
+      '&.Mui-focused fieldset': {
+        borderColor: 'gray'
+      }
+    },
+    '& label.Mui-focused': {
+      color: theme.palette.type === 'dark' && theme.palette.common.white
+    }
+  }
+}))
+
+const courseHandler = () => Promise.resolve(gql(`
+  query {
+    lessons {
+      _id
+      lessonCode
+      name
+      semester
+    }
+  }
+`).then((data) => data.lessons && data.lessons.sort(dynamicSortMultiple(
+  'semester',
+  'lessonCode'
+))))
+
+const notesHandler = (courseId) => Promise.resolve(gql(`
+  query {
+    lessonNotes(lesson: "${courseId}") {
+      _id
+      date
+      hypertexts
+      title
+      images {
+        url
+        details {
+          url
+          width
+          height
+        }
+      }
+    }
+  }
+`).then((data) => data.lessonNotes && data.lessonNotes.sort(dynamicSortMultiple(
+  '-date',
+  'title'
+))))
+
+export default function NotesPage (props) {
+  const classes = useStyles()
+  const [
+    lessons,
+    setLessons
+  ] = useState([])
+  const [
+    selectedLesson,
+    setSelectedLesson
+  ] = useState(null)
+  const [
+    notes,
+    setNotes
+  ] = useState([])
+  const [
+    dialog,
+    setDialog
+  ] = React.useState({ open: false, title: '', texts: [], image: [] })
+
+  function setOpen (open) {
+    setDialog({ ...dialog, open })
+  }
+
+  function handleClickOpen (title, texts, images) {
+    // eslint-disable-next-line func-names
+    return function () {
+      setDialog({ open: true, title, texts, images })
+    }
+  }
+
+  useEffect(
+    () => {
+      props.updateTitle('Notes')
+      courseHandler().then((gqlLessons) => {
+        if (gqlLessons) {
+          setLessons(gqlLessons)
+        }
+      })
+    },
+    []
+  )
+
+  function changeHandler (event, newValue) {
+    setSelectedLesson(newValue)
+    if (newValue) {
+      notesHandler(newValue._id).then((gqlNotes) => {
+        if (gqlNotes) {
+          setNotes(gqlNotes)
+        }
+      })
+    } else {
+      setNotes([])
+    }
+  }
+
+  function getOptionLabelHandler (lesson) {
+    return lesson.name
+  }
+
+  function groupByHandler (lesson) {
+    return `Semester ${lesson.semester}`
+  }
+
+  function renderInputHandler (params) {
+    return (
+      <TextField
+        {...params}
+        label="Course"
+        variant="outlined"
+      />
+    )
+  }
+
+  return (
+    <Container>
+      <Autocomplete
+        className={classes.autocomplete}
+        fullWidth
+        getOptionLabel={getOptionLabelHandler}
+        groupBy={groupByHandler}
+        id="notes"
+        onChange={changeHandler}
+        options={lessons}
+        renderInput={renderInputHandler}
+        value={selectedLesson}
+      />
+      {
+        notes && notes.map((note, index) => (
+          <Card
+            className={classes.root}
+            // eslint-disable-next-line react/no-array-index-key
+            key={`hypertext-${index}`}
+          >
+            <CardActionArea onClick={handleClickOpen(
+              note.title,
+              note.hypertexts,
+              note.images
+            )}
+            >
+              <CardContent>
+                <Typography
+                  className={classes.title}
+                  color="textSecondary"
+                  gutterBottom
+                >
+                  {note.date}
+                </Typography>
+                <Typography
+                  component="h2"
+                  variant="h5"
+                />
+                <Typography
+                  component="p"
+                  variant="body2"
+                >
+                  {note.title}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))
+      }
+      <Dialog
+        images={dialog.images}
+        open={dialog.open}
+        setOpen={setOpen}
+        texts={dialog.texts}
+        title={dialog.title}
+      />
+    </Container>
+  )
+}
