b3577e775e03422a709a20f107e50456d8740c46
CrazyStevenz <spapanatsios@gmail.com>
2020-12-16 02:37:08 +0200

    Add defaults and more functions to institutesSlice and fix bugs

--- redux/institutesSlice.js
+++ redux/institutesSlice.js
@@ -1,12 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit'
 import gql from '../scripts/graphql'
 
+function defaults() {
+  return { institutes: [], instituteIndex: -1 }
+}
+
 const slice = createSlice({
   name: 'institutes',
-  initialState: {},
+  initialState: defaults(),
   reducers: {
     updateInstitutes: (state, action) => ({
-      ...action.payload
+      ...defaults(),
+      institutes: action.payload
+    }),
+    updateInstituteIndex: (state, action) => ({
+      ...state,
+      instituteIndex: action.payload
     })
   }
 })
@@ -16,7 +25,7 @@ export default slice.reducer
 export function getInstitutes() {
   return (dispatch, getState) => {
     const stateBefore = getState()
-    if (stateBefore.institutes.length) return
+    if (stateBefore.institutes.institutes.length) return
 
     Promise.resolve(
       gql(`
@@ -50,6 +59,20 @@ export function getInstitutes() {
   }
 }
 
+export function updateInstituteIndex(index) {
+  return (dispatch) => {
+    dispatch(slice.actions.updateInstituteIndex(index))
+  }
+}
+
 export function selectInstitutes(state) {
-  return state.institutes
+  return state.institutes.institutes
+}
+
+export function selectInstitute(state) {
+  return state.institutes.institutes[state.institutes.instituteIndex]
+}
+
+export function selectInstituteIndex(state) {
+  return state.institutes.instituteIndex
 }
