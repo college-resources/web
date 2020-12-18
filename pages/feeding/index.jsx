4f6bc2e609c8a455c56146e22f322a23e0aa4291
CrazyStevenz <spapanatsios@gmail.com>
2020-12-19 01:06:53 +0200

    Add helpful message when no institutes are selected in feeding page

--- pages/feeding/index.jsx
+++ pages/feeding/index.jsx
@@ -4,6 +4,8 @@ import { useSelector } from 'react-redux'
 import Feeding from 'components/feeding/Feeding'
 import InstituteSelect from 'components/InstituteSelect'
 import { selectInstituteIndex } from 'redux/instituteSlice'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
 
 export default function FeedingPage(props) {
   const selectedInstituteIndex = useSelector(selectInstituteIndex)
@@ -15,7 +17,15 @@ export default function FeedingPage(props) {
   return (
     <Container>
       <InstituteSelect />
-      {selectedInstituteIndex >= 0 && <Feeding />}
+      {selectedInstituteIndex < 0 ? (
+        <Box mt={5}>
+          <Typography align="center">
+            Select an institute from the dropdown to see its restaurants.
+          </Typography>
+        </Box>
+      ) : (
+        <Feeding />
+      )}
     </Container>
   )
 }
