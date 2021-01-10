25644ec2f78cf7e922b6a3a013b9fea55b608fa3
jim3692 <31220180+jim3692@users.noreply.github.com>
2021-01-10 15:01:41 +0200

    Move sessions to Firestore and remove random session key (#69)

--- server/middleware/session.js
+++ server/middleware/session.js
@@ -1,10 +1,18 @@
 const session = require('express-session')
+const {FirestoreStore} = require('@google-cloud/connect-firestore')
+const {Firestore} = require('@google-cloud/firestore')
 
 const sess = {
+  store: process.env.NODE_ENV === 'production'
+    ? new FirestoreStore({
+      dataset: new Firestore(),
+      kind: process.env.SESSION_COLLECTION || 'local-sessions',
+    })
+    : undefined,
   cookie: {},
   resave: false,
   saveUninitialized: true,
-  secret: require('crypto').randomBytes(64).toString('hex')
+  secret: process.env.SESSION_SECRET || 'local-sessions'
 }
 
 module.exports = session(sess)
