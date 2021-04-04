9e3cf90a4aac80755e470f5756ea1c68834a1c8e
CrazyStevenz <spapanatsios@gmail.com>
2021-04-04 21:26:58 +0200

    Add @next/bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
-
-module.exports = withPWA({
-  pwa: {
-    disable: true,
-    register: true,
-    dest: 'public',
-    runtimeCaching
-  },
-  crossOrigin: 'anonymous',
-  pageExtensions: ['js', 'jsx']
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
 })
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    pwa: {
+      disable: true,
+      register: true,
+      dest: 'public',
+      runtimeCaching
+    },
+    crossOrigin: 'anonymous',
+    pageExtensions: ['js', 'jsx']
+  })
+)
