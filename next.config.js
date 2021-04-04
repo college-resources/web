ad386e5d7786751365fba2643f1abdd04464b849
CrazyStevenz <spapanatsios@gmail.com>
2021-04-04 20:39:03 +0200

    Disable pwa

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   pwa: {
-    disable: process.env.NODE_ENV === 'development',
+    disable: true,
     register: true,
     dest: 'public',
     runtimeCaching
